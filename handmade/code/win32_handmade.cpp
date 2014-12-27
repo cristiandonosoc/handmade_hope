7df1c5d920f47ada89ef3c34b5cd2dffb4b0aa23
cristiandonosoc <cristiandonosoc@gmail.com>
2014-12-27 16:42:39 -0300

    Day 001

---
+++ handmade/code/win32_handmade.cpp
@@ -0,0 +1,11 @@
+#include <windows.h>
+
+int CALLBACK
+WinMain(HINSTANCE hInstance,
+        HINSTANCE hPrevInstance,
+        LPSTR lpCmdLine,
+        int nCmdShow)
+{
+  MessageBox( 0, "This is Handmade Hope", "Handmade Hope", MB_OK|MB_ICONINFORMATION);
+  return(0);
+}
