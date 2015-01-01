be1bc2d92f0ce7863f8f1d3f8eca1180acc0fbdf
cristiandonosoc <cristiandonosoc@gmail.com>
2015-01-01 20:16:18 -0300

    Day 005: Refactoring and Revision of the windows code

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -26,19 +26,48 @@ typedef int16_t int16;
 typedef int32_t int32;
 typedef int64_t int64;
 
+struct Win32OffScreenBuffer
+{
+  BITMAPINFO info;
+  void *memory;
+  int width;
+  int height;
+  int pitch;
+};
+
+struct Win32WindowDimension {
+  int width;
+  int height;
+};
+
+internal Win32WindowDimension
+Win32GetWindowDimension(HWND windowHandle)
+{
+  Win32WindowDimension result;
+
+  RECT clientRect;
+  GetClientRect(windowHandle, &clientRect);
+  result.width = clientRect.right - clientRect.left;
+  result.height = clientRect.bottom - clientRect.top;
+
+  return(result);
+}
+
 //TODO:(Cristián): This is a global for now
-global_variable bool RUNNING;
-global_variable BITMAPINFO bitmapInfo;
-global_variable void *bitmapMemory;
-global_variable int bitmapWidth;
-global_variable int bitmapHeight;
-global_variable int bytesPerPixel = 4;
+global_variable bool gRunning;
+global_variable Win32OffScreenBuffer gBackBuffer;
 
 internal void
-Win32RenderWeirdGradient(int xOffset, int yOffset)
+Win32RenderWeirdGradient(Win32OffScreenBuffer buffer, int blueOffset, int greenOffset)
 {
-  int pitch = bitmapWidth * bytesPerPixel;
-  uint8* row = (uint8 *)bitmapMemory;
+  // TODO(Cristián): Let's see what the optimizer does if it is passed by reference
+  // instead of a pointer.
+  int bitmapWidth = buffer.width;
+  int bitmapHeight = buffer.height;
+  int pitch = buffer.pitch;
+  void *memory = buffer.memory;
+
+  uint8* row = (uint8 *)memory;
   for(int y = 0;
       y < bitmapHeight;
       y++)
@@ -53,12 +82,12 @@ Win32RenderWeirdGradient(int xOffset, int yOffset)
         Little Endian Architecture
         Pixel in Register: 0xXXRRGGBB
        */
-      uint8 blue = (x + xOffset);
-      uint8 green = (y + yOffset);
+      uint8 blue = (x + blueOffset);
+      uint8 green = (y + greenOffset);
       uint8 red = 0;
       uint8 pad = 0;
 
-      *pixel++ = (green << 8) | blue;
+      *pixel++ = blue | (green << 8);
     }
     row += pitch;
   }
@@ -66,30 +95,35 @@ Win32RenderWeirdGradient(int xOffset, int yOffset)
 
 /**
  * (Re)Creates a Device Independent Bitmap to match the current window size
+ * @param buffer  A pointer to the Win32OffScreenBuffer. It is important that it is
+ *                a pointer because it will modify the buffer and the changes must endure.
+ *                (This makes it difficult to the compiler to inline, but is les error-prone).
  */
 internal void
-ResizeDIBSection(int width, int height)
+Win32ResizeDIBSection(Win32OffScreenBuffer *buffer, int width, int height)
 {
   //TODO(Cristián): Bulletproof this
   // Maybe don't free first, free after, then free first if that fails
 
   // TODO(Cristián): Free our DIBSection
   // We check whether we have created a bitmap
-  if(bitmapMemory)
+  if(buffer->memory)
   {
-    VirtualFree(bitmapMemory, 0, MEM_RELEASE);
+    VirtualFree(buffer->memory, 0, MEM_RELEASE);
   }
 
-  bitmapWidth = width;
-  bitmapHeight = height;
+  // We update the buffer width
+  buffer->width = width;
+  buffer->height = height;
+  int bytesPerPixel = 4;
 
   // We set the BITMAPINFO data
-  bitmapInfo.bmiHeader.biSize = sizeof(bitmapInfo.bmiHeader);
-  bitmapInfo.bmiHeader.biWidth = bitmapWidth;
-  bitmapInfo.bmiHeader.biHeight = -bitmapHeight; // Negative height means top down bitmap (BITMAPINFO MSDN)
-  bitmapInfo.bmiHeader.biPlanes = 1;
-  bitmapInfo.bmiHeader.biBitCount = 32;
-  bitmapInfo.bmiHeader.biCompression = BI_RGB;
+  buffer->info.bmiHeader.biSize = sizeof(buffer->info.bmiHeader);
+  buffer->info.bmiHeader.biWidth = width;
+  buffer->info.bmiHeader.biHeight = -height; // Negative height means top down bitmap (BITMAPINFO MSDN)
+  buffer->info.bmiHeader.biPlanes = 1;
+  buffer->info.bmiHeader.biBitCount = 32;
+  buffer->info.bmiHeader.biCompression = BI_RGB;
 
   // NOTE(Cristián): There was a confusion on whether to use StretchDIBits or
   // BitBlt for passing the bitmap from our program to windows. The second method
@@ -98,29 +132,33 @@ ResizeDIBSection(int width, int height)
   // and maintain the memory ourselves.
 
   // We allocate the memory necesary for the buffer
-  int bitmapMemorySize = bytesPerPixel * bitmapWidth * bitmapHeight;
-  bitmapMemory = VirtualAlloc(0, bitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+  int bitmapMemorySize =  bytesPerPixel *
+                          buffer->width *
+                          buffer->height;
+  buffer->memory = VirtualAlloc(0, bitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+  buffer->pitch = buffer->width * bytesPerPixel;
 
   //TODO:(Cristián): Probably clear screen to black
 }
 
 internal void
-Win32UpdateWindow(HDC deviceContext, RECT *clientRect, int x, int y, int width, int height)
+Win32TransferBufferToWindows(HDC deviceContext,
+                             Win32OffScreenBuffer buffer,
+                             int windowWidth, int windowHeight)
 {
+  // TODO(Cristián): aspect ratio correction
+  // TODO(Cristián): Play with stretch mode
   // NOTE(Cristián): We are avoiding dirty (partial) rectangles for now
-  int windowWidth = clientRect->right - clientRect->left;
-  int windowHeight = clientRect->bottom - clientRect->top;
-
   StretchDIBits(
     deviceContext,
     /*
-    x, y, width, height,
-    x, y, width, height,
+    x, y, destWidth, destHeight,
+    x, y, originWidth, originHeight,
     */
-    0, 0, bitmapWidth, bitmapHeight,
     0, 0, windowWidth, windowHeight,
-    bitmapMemory,
-    &bitmapInfo,
+    0, 0, buffer.width, buffer.height,
+    buffer.memory,
+    &buffer.info,
     DIB_RGB_COLORS, SRCCOPY);
 }
 
@@ -129,9 +167,9 @@ Win32UpdateWindow(HDC deviceContext, RECT *clientRect, int x, int y, int width, 
  */
 LRESULT CALLBACK
 Win32MainWindowCallback(HWND windowHandle,
-  UINT message,
-  WPARAM wParam,
-  LPARAM lParam
+                        UINT message,
+                        WPARAM wParam,
+                        LPARAM lParam
 )
 {
   LRESULT result = 0;
@@ -139,11 +177,6 @@ Win32MainWindowCallback(HWND windowHandle,
   {
     case WM_SIZE:
       {
-        RECT clientRect;
-        GetClientRect(windowHandle, &clientRect);
-        int width = clientRect.right - clientRect.left;
-        int height = clientRect.bottom - clientRect.top;
-        ResizeDIBSection(width, height);
       } break;
     case WM_ACTIVATEAPP:
       {
@@ -152,27 +185,32 @@ Win32MainWindowCallback(HWND windowHandle,
     case WM_CLOSE:
       {
         //TODO:(Cristián): Handle this with a message to the user
-        RUNNING = false;
+        gRunning = false;
       } break;
     case WM_DESTROY:
       {
         //TODO:(Cristián): Handle this with an error - Recreate window?
-        RUNNING = false;
+        gRunning = false;
       } break;
     case WM_PAINT:
       {
+        // We get the windows paint device context (and its dimensions)
         PAINTSTRUCT paint;
-        // We paint a black n' white epillepsy!
         HDC deviceContext = BeginPaint(windowHandle, &paint);
-        int x = paint.rcPaint.left;
-        int y = paint.rcPaint.top;
-        int width = paint.rcPaint.right - paint.rcPaint.left;
-        int height = paint.rcPaint.bottom - paint.rcPaint.top;
-
-        RECT clientRect;
-        GetClientRect(windowHandle, &clientRect);
-
-        Win32UpdateWindow(deviceContext, &clientRect, x, y, width, height);
+        /*
+          // We do not care for the dirty rectangle because this is a game
+          // that renders the whole screen each frame. Otherwise, we could
+          // use the paint parameters to just update the part of the window
+          // that needs to be repainted.
+          int x = paint.rcPaint.left;
+          int y = paint.rcPaint.top;
+          int width = paint.rcPaint.right - paint.rcPaint.left;
+          int height = paint.rcPaint.bottom - paint.rcPaint.top;
+        */
+        Win32WindowDimension dimension = Win32GetWindowDimension(windowHandle);
+        Win32TransferBufferToWindows(deviceContext,
+                                     gBackBuffer,
+                                     dimension.width, dimension.height);
         EndPaint(windowHandle, &paint);
       } break;
     default:
@@ -190,30 +228,20 @@ Win32MainWindowCallback(HWND windowHandle,
  */
 int CALLBACK
 WinMain(HINSTANCE hInstance,
-    HINSTANCE prevhInstance,
-    LPSTR commandLine,
-    int showCode)
+        HINSTANCE prevHInstance,
+        LPSTR commandLine,
+        int showCode)
 {
   // In C++, 0 Initialization of a struct is as follows
   WNDCLASS windowClass = {};
-  /* The WNDCLASS struct definition
-     UINT      style;
-     WNDPROC   lpfnWndProc;
-     int       cbClsExtra;
-     int       cbWndExtra;
-     HINSTANCE hInstance;
-     HICON     hIcon;
-     HCURSOR   hCursor;
-     HBRUSH    hbrBackground;
-     LPCTSTR   lpszMenuName;
-     LPCTSTR   lpszClassName;
-     */
-  //TODO(Cristián): Check if CS_HREDRAW and CS_VREDRAW still matter
-  windowClass.style = CS_OWNDC;
+  windowClass.style = CS_HREDRAW|CS_VREDRAW|CS_OWNDC;
   windowClass.lpfnWndProc = Win32MainWindowCallback;
   windowClass.hInstance = hInstance;
   //windowClass.hIcon;
   windowClass.lpszClassName = "HandmadeHopeWindowClass";
+
+  // We setup the backbuffer
+  Win32ResizeDIBSection(&gBackBuffer, 1280, 740);
 
   if(RegisterClassA(&windowClass))
   {
@@ -233,36 +261,52 @@ WinMain(HINSTANCE hInstance,
     );
     if(windowHandle)
     {
+      // NOTE(Cristián): Sice we specified CS_OWNDC for our WNDCLASS, we can just
+      // get one device context and use it forever, because we don't need to return it.
+      HDC deviceContext = GetDC(windowHandle);
       // ** MESSAGE LOOP **
       // We retrieve the messages from windows via the message queue
-      RUNNING = true;
-      int xOffset = 0;
-      int yOffset = 0;
-      while(RUNNING)
+      int blueOffset = 0;
+      int greenOffset = 0;
+      gRunning = true;
+      while(gRunning)
       {
-        // Windows may call the callback even outside the loop
+        /**
+         * We create the MSG inside the loop instead of outside to use correct
+         * 'lexical' scoping. This means we protect outselves from referencing this pointer
+         * outside this scope, which would be potentially a bug because this MSG only makes
+         * sense in this loop.
+         *
+         * We can do this because the MSG is a simple structure, so the compiler will (probably)
+         * optimize it in such a way it should not make much difference whether it is defined
+         * inside the scope or outside it.
+         *
+         * It is importante to note that is we use C++ constructors/destructors IT DOES MAKE
+         * A DIFFERENCE whether we put it in a loop because the spec requires it to call those
+         * callbacks everytime the structure (or class) is created/destroyed. This can mean a lot
+         * of code of overhead if defined inside the loop. But this is not such case.
+         */
         MSG message;
+        // Windows may call the callback even outside the loop
         while(PeekMessageA(&message, 0, 0, 0, PM_REMOVE))
         {
           if(message.message == WM_QUIT)
           {
-            RUNNING = false;
+            gRunning = false;
           }
 
           TranslateMessage(&message);
           DispatchMessageA(&message);
         }
 
-        HDC deviceContext = GetDC(windowHandle);
-        RECT clientRect;
-        GetClientRect(windowHandle, &clientRect);
-        int windowWidth = clientRect.right - clientRect.left;
-        int windowHeight = clientRect.bottom - clientRect.top;
+        Win32RenderWeirdGradient(gBackBuffer, blueOffset, greenOffset);
 
-        Win32RenderWeirdGradient(xOffset, yOffset);
-        Win32UpdateWindow(deviceContext, &clientRect, 0, 0, windowWidth, windowHeight);
-        ReleaseDC(windowHandle, deviceContext);
-        xOffset++;
+        Win32WindowDimension dimension = Win32GetWindowDimension(windowHandle);
+        Win32TransferBufferToWindows(deviceContext,
+                                     gBackBuffer,
+                                     dimension.width, dimension.height);
+        blueOffset++;
+        greenOffset += 2;
       }
     }
     else
