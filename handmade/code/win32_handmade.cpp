39f305757f231c3ba4973039153b11f9677036b0
cristiandonosoc <cristiandonosoc@gmail.com>
2015-01-02 12:12:49 -0300

    Day 006: Added XInput X360 controller support

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -9,6 +9,7 @@
 
 #include <windows.h>
 #include <stdint.h>
+#include <xinput.h>
 
 // We rename static to some aliases to make more transparent the use of each
 #define internal static           // Makes functions scoped to the 'translation unit'
@@ -26,7 +27,7 @@ typedef int16_t int16;
 typedef int32_t int32;
 typedef int64_t int64;
 
-struct Win32OffScreenBuffer
+struct win32_offscreen_buffer
 {
   BITMAPINFO info;
   void *memory;
@@ -35,15 +36,59 @@ struct Win32OffScreenBuffer
   int pitch;
 };
 
-struct Win32WindowDimension {
+struct win32_window_dimension {
   int width;
   int height;
 };
 
-internal Win32WindowDimension
+/**
+ * We make a windows binding ourselves. We this we bypass an import librabry.
+ * We do this in the case of XInput because the required library changes according to
+ * the version of Windows the user is using, so we would like the game to function
+ * independent of the dll that's really required.
+ *
+ * What we do is we define types that serve as pointers to the functions we are trying
+ * to override. The we define global variables of such types, so we have global pointers
+ * that point to the functions in the Windows API.
+ * Then we define there global variables as the real meaning of XInput methods, so we
+ * never use the ones defined in xinput.h
+ */
+// We define a macro that generates the methods definition
+#define X_INPUT_GET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_STATE *pState)
+#define X_INPUT_SET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_VIBRATION *pVibration)
+// We generate the types for the pointers of the methods
+typedef X_INPUT_GET_STATE(x_input_get_state);
+typedef X_INPUT_SET_STATE(x_input_set_state);
+// We generate stub functions for those methods
+X_INPUT_GET_STATE(XInputGetStateStub) { return(0); }
+X_INPUT_SET_STATE(XInputSetStateStub) { return(0); }
+// We generate global pointers to the functions and at first we assign them
+// to the harmless stubs. This way the game won't crash if the dll is not loaded.
+global_variable x_input_get_state *DynamicXInputGetState = XInputGetStateStub;
+global_variable x_input_set_state *DynamicXInputSetState = XInputSetStateStub;
+#define XInputGetState DynamicXInputGetState
+#define XInputSetState DynamicXInputSetState
+
+/**
+ * Tries to load the XInput libraries
+ */
+internal void
+Win32LoadXInput()
+{
+  // We use xinput1_3.dll because it is more widespread installed
+  // in Windows machines than xinput1_3.dll
+  HMODULE XInputLibrary = LoadLibrary("xinput1_3.dll");
+  if(XInputLibrary)
+  {
+    XInputGetState = (x_input_get_state *)GetProcAddress(XInputLibrary, "XInputGetState");
+    XInputSetState = (x_input_set_state *)GetProcAddress(XInputLibrary, "XInputSetState");
+  }
+}
+
+internal win32_window_dimension
 Win32GetWindowDimension(HWND windowHandle)
 {
-  Win32WindowDimension result;
+  win32_window_dimension result;
 
   RECT clientRect;
   GetClientRect(windowHandle, &clientRect);
@@ -55,10 +100,10 @@ Win32GetWindowDimension(HWND windowHandle)
 
 //TODO:(Cristián): This is a global for now
 global_variable bool gRunning;
-global_variable Win32OffScreenBuffer gBackBuffer;
+global_variable win32_offscreen_buffer gBackBuffer;
 
 internal void
-Win32RenderWeirdGradient(Win32OffScreenBuffer buffer, int blueOffset, int greenOffset)
+Win32RenderWeirdGradient(win32_offscreen_buffer buffer, int blueOffset, int greenOffset)
 {
   // TODO(Cristián): Let's see what the optimizer does if it is passed by reference
   // instead of a pointer.
@@ -95,12 +140,12 @@ Win32RenderWeirdGradient(Win32OffScreenBuffer buffer, int blueOffset, int greenO
 
 /**
  * (Re)Creates a Device Independent Bitmap to match the current window size
- * @param buffer  A pointer to the Win32OffScreenBuffer. It is important that it is
+ * @param buffer  A pointer to the win32_offscreen_buffer. It is important that it is
  *                a pointer because it will modify the buffer and the changes must endure.
  *                (This makes it difficult to the compiler to inline, but is les error-prone).
  */
 internal void
-Win32ResizeDIBSection(Win32OffScreenBuffer *buffer, int width, int height)
+Win32ResizeDIBSection(win32_offscreen_buffer *buffer, int width, int height)
 {
   //TODO(Cristián): Bulletproof this
   // Maybe don't free first, free after, then free first if that fails
@@ -143,7 +188,7 @@ Win32ResizeDIBSection(Win32OffScreenBuffer *buffer, int width, int height)
 
 internal void
 Win32TransferBufferToWindows(HDC deviceContext,
-                             Win32OffScreenBuffer buffer,
+                             win32_offscreen_buffer buffer,
                              int windowWidth, int windowHeight)
 {
   // TODO(Cristián): aspect ratio correction
@@ -207,7 +252,7 @@ Win32MainWindowCallback(HWND windowHandle,
           int width = paint.rcPaint.right - paint.rcPaint.left;
           int height = paint.rcPaint.bottom - paint.rcPaint.top;
         */
-        Win32WindowDimension dimension = Win32GetWindowDimension(windowHandle);
+        win32_window_dimension dimension = Win32GetWindowDimension(windowHandle);
         Win32TransferBufferToWindows(deviceContext,
                                      gBackBuffer,
                                      dimension.width, dimension.height);
@@ -232,6 +277,9 @@ WinMain(HINSTANCE hInstance,
         LPSTR commandLine,
         int showCode)
 {
+  // We initialize the XInput functions pointers
+  Win32LoadXInput();
+
   // In C++, 0 Initialization of a struct is as follows
   WNDCLASS windowClass = {};
   windowClass.style = CS_HREDRAW|CS_VREDRAW|CS_OWNDC;
@@ -299,14 +347,70 @@ WinMain(HINSTANCE hInstance,
           DispatchMessageA(&message);
         }
 
+        // Xinput is a polling based API
+        // TODO(Cristián): Should we pull this more frequently?
+        for(DWORD controllerIndex = 0;
+            controllerIndex < XUSER_MAX_COUNT;
+            controllerIndex++)
+        {
+          DWORD result;
+          XINPUT_STATE controllerState;
+
+          if(XInputGetState(controllerIndex, &controllerState) == ERROR_SUCCESS) // Amazing success key code name
+          {
+            // TODO(Cristián): See if controllerState.swPacketNumber incrementes too rapidly
+            // We import the whole gamepad state
+            XINPUT_GAMEPAD *pad = &controllerState.Gamepad;
+            bool up = (pad->wButtons & XINPUT_GAMEPAD_DPAD_UP);
+            bool down = (pad->wButtons & XINPUT_GAMEPAD_DPAD_DOWN);
+            bool left = (pad->wButtons & XINPUT_GAMEPAD_DPAD_LEFT);
+            bool right = (pad->wButtons & XINPUT_GAMEPAD_DPAD_RIGHT);
+            bool start = (pad->wButtons & XINPUT_GAMEPAD_START);
+            bool back = (pad->wButtons & XINPUT_GAMEPAD_BACK);
+            bool leftThumb = (pad->wButtons & XINPUT_GAMEPAD_LEFT_THUMB);
+            bool rightThumb = (pad->wButtons & XINPUT_GAMEPAD_RIGHT_THUMB);
+            bool leftShoulder = (pad->wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER);
+            bool rightShoulder = (pad->wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER);
+            bool aButton = (pad->wButtons & XINPUT_GAMEPAD_A);
+            bool bButton = (pad->wButtons & XINPUT_GAMEPAD_B);
+            bool yButton = (pad->wButtons & XINPUT_GAMEPAD_Y);
+            bool xButton = (pad->wButtons & XINPUT_GAMEPAD_X);
+
+            // We inport the sticks
+            int16 leftThumbX = pad->sThumbLX;
+            int16 leftThumbY = pad->sThumbLY;
+            int16 rightThumbX = pad->sThumbRX;
+            int16 rightThumbY = pad->sThumbRY;
+
+            // We import the triggers
+            int16 leftTrigger = pad->bLeftTrigger;
+            int16 rightTrigger = pad->bRightTrigger;
+
+            // We assign acceleration
+            blueOffset += leftThumbX / 4096;
+            greenOffset -= leftThumbY / 4096; // We invert the Y because the screen is also inverted
+
+            XINPUT_VIBRATION xInputVibration = {};
+            xInputVibration.wLeftMotorSpeed = aButton ? 65535 : 0;
+            xInputVibration.wRightMotorSpeed = bButton ? 65535 : 0;
+
+            XInputSetState(controllerIndex, &xInputVibration);
+          }
+          else
+          {
+            // NOTE(Cristián): The controller is not available
+          }
+ 
+        }
+
         Win32RenderWeirdGradient(gBackBuffer, blueOffset, greenOffset);
 
-        Win32WindowDimension dimension = Win32GetWindowDimension(windowHandle);
+        win32_window_dimension dimension = Win32GetWindowDimension(windowHandle);
         Win32TransferBufferToWindows(deviceContext,
                                      gBackBuffer,
                                      dimension.width, dimension.height);
-        blueOffset++;
-        greenOffset += 2;
+        //blueOffset++;
+        //greenOffset += 2;
       }
     }
     else
