8ec4e82a9d5a82c21a2cda3eaf43fb3c610f90a4
Cristián Donoso C <cristiandonosoc@gmail.com>
2015-07-22 18:34:36 -0300

    Updated shell.bat to open Visual Studio on start

--- handmade/misc/shell.bat
+++ handmade/misc/shell.bat
@@ -1,3 +1,9 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 set path=w:\handmade\misc\;%path%
+
+REM We start the devenv
+pushd ..\..\build
+devenv win32_handmade.exe
+popd
+cd w:\handmade\code
