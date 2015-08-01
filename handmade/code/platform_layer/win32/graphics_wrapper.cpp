421dc77f171fa1415f2ed81e6b57b37035bc1545
Cristián Donoso C <cristiandonosoc@gmail.com>
2015-08-01 11:23:22 -0300

    Added black window clearing and double window sampling

--- handmade/code/platform_layer/win32/graphics_wrapper.cpp
+++ handmade/code/platform_layer/win32/graphics_wrapper.cpp
@@ -119,25 +119,53 @@ Win32TransferBufferToWindows(HDC deviceContext,
                              win32_offscreen_buffer *buffer,
                              int windowWidth, int windowHeight)
 {
-  int offsetX = 10;
-  int offsetY = 10;
+  if((windowWidth >= 2 * buffer->width) &&
+     (windowHeight >= 2 * buffer->height))
+  {
+    // If we can double stretch the buffer, we do it, because double-sampling
+    // will not produce artifacts with StretchDIBits
+    StretchDIBits(deviceContext,
+                  /*
+                  x, y, destWidth, destHeight,
+                  x, y, originWidth, originHeight,
+                  */
+                  0, 0, 2 * buffer->width, 2 * buffer->height,
+                  0, 0, buffer->width, buffer->height,
+                  buffer->memory,
+                  &buffer->info,
+                  DIB_RGB_COLORS,
+                  SRCCOPY);
+  }
+  else
+  {
+    int offsetX = 10;
+    int offsetY = 10;
 
-  // NOTE(Cristián): We are avoiding dirty (partial) rectangles for now
-  // NOTE(Cristián): For prototyping, we're are always going to blit 1-1 pixels
-  // to make sure we don't introduce artifacts. This will change when the renderer
-  // code is done.
-  StretchDIBits(deviceContext,
-                /*
-                x, y, destWidth, destHeight,
-                x, y, originWidth, originHeight,
-                */
-                // 0, 0, windowWidth, windowHeight,
-                offsetX, offsetY, buffer->width, buffer->height,
-                0, 0, buffer->width, buffer->height,
-                buffer->memory,
-                &buffer->info,
-                DIB_RGB_COLORS,
-                SRCCOPY);
+    // We have to clean the rectangles outside of our render target rectangle
+    PatBlt(deviceContext, 0, 0, windowWidth, offsetY, BLACKNESS);
+    PatBlt(deviceContext, 0, 0, offsetX, windowHeight, BLACKNESS);
+    PatBlt(deviceContext, offsetX, offsetY + buffer->height,
+                          windowWidth - offsetX, windowHeight - (offsetY + buffer->height),
+                          BLACKNESS);
+    PatBlt(deviceContext, offsetX + buffer->width, offsetY,
+                          windowWidth - (offsetX + buffer->width), windowHeight - offsetY,
+                          BLACKNESS);
+
+    // NOTE(Cristián): For prototyping, we're are always going to blit 1-1 pixels
+    // to make sure we don't introduce artifacts. This will change when the renderer
+    // code is done.
+    StretchDIBits(deviceContext,
+                  /*
+                  x, y, destWidth, destHeight,
+                  x, y, originWidth, originHeight,
+                  */
+                  offsetX, offsetY, buffer->width, buffer->height,
+                  0, 0, buffer->width, buffer->height,
+                  buffer->memory,
+                  &buffer->info,
+                  DIB_RGB_COLORS,
+                  SRCCOPY);
+  }
 }
 
 #define _WIN32_GRAPHICS_WRAPPER_INCLUDED
