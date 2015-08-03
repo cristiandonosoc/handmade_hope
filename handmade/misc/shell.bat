c3bd5db7e617d382df92b160d754d19346d0a70d
Cristián Donoso <cristiandonosoc@gmail.com>
2015-08-02 21:47:48 -0300

    Added 14 and 12 visual studio loading

--- handmade/misc/shell.bat
+++ handmade/misc/shell.bat
@@ -1,5 +1,10 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
+  call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+) else (
+  call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+)
+
 set path=w:\handmade\misc\;%path%
 
 REM We start the devenv
